7b9d7fd47dba7b50f3e19ecdc65eb478499c81df
Ben Altschuler <baltschuler@Bens-MBP-5.home>
2020-11-26 09:50:22 -0500

    Extra time bugs fixed, ready for push

--- inductionApp/SignupView.swift
+++ inductionApp/SignupView.swift
@@ -43,13 +43,13 @@ struct SignupView: View {
                     
                     HStack {
                         VStack {
-                            FormField(fieldName: "Password", fieldValue: $userRegistrationViewModel.password, isSecure: false) //true
+                            FormField(fieldName: "Password", fieldValue: $userRegistrationViewModel.password, isSecure: true) //true
                             RequirementText(iconName: "lock.open", iconColor: userRegistrationViewModel.isPasswordLengthValid ? Color.secondary : Color(red: 251/255, green: 128/255, blue: 128/255), text: "A minimum of 8 characters", isStrikeThrough: userRegistrationViewModel.isPasswordLengthValid)
                             RequirementText(iconName: "lock.open", iconColor: userRegistrationViewModel.isPasswordCapitalLetter ? Color.secondary : Color(red: 251/255, green: 128/255, blue: 128/255), text: "One uppercase letter", isStrikeThrough: userRegistrationViewModel.isPasswordCapitalLetter)
                         }
                         .padding()
                         VStack {
-                            FormField(fieldName: "Confirm Password", fieldValue: $userRegistrationViewModel.passwordConfirm, isSecure: false) //true
+                            FormField(fieldName: "Confirm Password", fieldValue: $userRegistrationViewModel.passwordConfirm, isSecure: true) //true
                             RequirementText(iconColor: userRegistrationViewModel.isPasswordCapitalLetter ? Color.secondary : Color(red: 251/255, green: 128/255, blue: 128/255), text: "Your confirm password should be the same as password", isStrikeThrough: userRegistrationViewModel.isPasswordConfirmValid)
                                 .padding()
                             
